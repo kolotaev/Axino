2ee0db3919140b35b27083a73a8b8a23c1cfa9bf
kolotaev <kolotaev@tut.by>
2019-04-30 13:35:18 +0300

    Eslint use 4 spaces even for airbnb style

--- proxy.js
+++ proxy.js
@@ -1,13 +1,13 @@
 const httpProxy = require('http-proxy');
-const http = require('http')
+const http = require('http');
 
-let proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer({});
 
-let server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     // You can define here your custom logic to handle the request
     // and then proxy the request.
     proxy.web(req, res, { target: 'http://127.0.0.1:80' });
 });
 
-console.log("listening on port 5050")
+console.log('listening on port 5050');
 server.listen(5050);
