339d1fa5982305cec02ec9be2849a2f3d0be1180
kolotaev <kolotaev@tut.by>
2019-10-23 03:20:20 +0300

    Fix tostring tests

--- test/unit/aggregate/aggregate_root_spec.js
+++ test/unit/aggregate/aggregate_root_spec.js
@@ -1,12 +1,19 @@
 const chai = require('chai').should();
 
-const AggregateRoot = require('../../../lib/aggregate');
+const { AggregateRoot } = require('../../../lib/aggregate');
 
 describe('AggregateRoot', () => {
   describe('#toString()', function() {
-    it('should return aggregate class name and ID string representation', () => {
-      // let ar = new AggregateRoot(123);
-      "jkj".should.to.equal('asdasd');
+    describe('should return aggregate class name and ID string representation', () => {
+      it('for base class', () => {
+        let ar = new AggregateRoot(123);
+        `${ar}`.should.to.equal('AggregateRoot: 123');
+      });
+      it('for custom class', () => {
+        let orderARClass = class OrderAggregateRoot extends AggregateRoot {}
+        let orderAR = new orderARClass('asdf');
+        `${orderAR}`.should.to.equal('OrderAggregateRoot: asdf');
+      });
     });
   });
 });
