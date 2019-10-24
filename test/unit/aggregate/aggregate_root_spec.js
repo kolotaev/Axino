40ab9cfb5e703f27e0291cbf12d33f0e2ff71f71
kolotaev <kolotaev@tut.by>
2019-10-24 14:37:09 +0300

    Add package bits

--- test/unit/aggregate/aggregate_root_spec.js
+++ test/unit/aggregate/aggregate_root_spec.js
@@ -7,12 +7,12 @@ describe('AggregateRoot', () => {
     describe('should return aggregate class name and ID string representation', () => {
       it('for base class', () => {
         let ar = new AggregateRoot(123);
-        `${ar}`.should.to.equal('AggregateRoot: 123');
+        `${ar}`.should.equal('AggregateRoot: 123');
       });
       it('for custom class', () => {
         let orderARClass = class OrderAggregateRoot extends AggregateRoot {}
         let orderAR = new orderARClass('asdf');
-        `${orderAR}`.should.to.equal('OrderAggregateRoot: asdf');
+        `${orderAR}`.should.equal('OrderAggregateRoot: asdf');
       });
     });
   });
